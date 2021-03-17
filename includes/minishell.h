236dd70c17a74ee86b8941cc1aa073677401b828
bahaas <baudoin.haas@gmail.com>
2021-03-17 14:14:52 +0100

    ADD: required lib in .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,17 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:28:05 by clorin            #+#    #+#             */
-/*   Updated: 2021/03/17 12:29:25 by clorin           ###   ########.fr       */
+/*   Updated: 2021/03/17 14:14:09 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
 # include "../libft/libft.h"
+
 
 #endif
