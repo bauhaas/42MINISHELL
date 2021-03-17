b75b4f9f6374491b3356442cdc80ef0337a27776
SIRIUS911 <cyrille.lorin@yahoo.fr>
2021-03-17 14:01:49 +0100

    ADD: main.c

--- srcs/main.c
+++ srcs/main.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
 
+int					main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	return (0);
+}
