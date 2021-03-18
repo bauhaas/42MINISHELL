b303b892dde63863d4728cde20b691760d74f397
bahaas <baudoin.haas@gmail.com>
2021-03-18 12:20:28 +0100

    ADD: Store all env in lst & endless prompt

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:28:05 by clorin            #+#    #+#             */
-/*   Updated: 2021/03/17 14:14:09 by bahaas           ###   ########.fr       */
+/*   Updated: 2021/03/18 12:17:38 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,17 @@
 # include <stdio.h>
 # include <unistd.h>
 # include "../libft/libft.h"
+
+typedef	struct	s_var
+{
+	char *name;
+	char *value;
+}				t_var;
+
+typedef struct	s_ms
+{
+	t_list	*env;
+}				t_ms;
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,8 +6,88 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:52:12 by clorin            #+#    #+#             */
-/*   Updated: 2021/03/17 12:52:24 by clorin           ###   ########.fr       */
+/*   Updated: 2021/03/18 12:17:22 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
 
+/*
+** Split the env variable string on the '=' sign, and set the var to the 1st
+** string of our split, the 2nd is the value. Making sure that our strings after
+** split exist.
+*/
+
+t_var	*init_var(char *env_var)
+{
+	char **split_var;
+	t_var *new;
+
+	new = malloc(sizeof(t_var));
+	if(!new)
+		return (NULL);
+	split_var = ft_split(env_var, '=');
+	if(split_var[0])
+		new->name=split_var[0];
+	else
+		new->name=NULL;
+	if(split_var[1])
+		new->value=split_var[1];
+	else
+		new->value=NULL;
+	return(new);
+}
+
+/*
+** Loop to create list of env variables. We init the t_var and then init
+** a new element in our list
+*/
+
+void list_env(t_list *lst_env, char **env)
+{
+	int i;
+	t_list *new;
+	t_var *var;
+	
+	i = -1;
+	new = NULL;
+	var = NULL;
+	while(env[++i])
+	{
+		var = init_var(env[i]);
+		new = ft_lstnew(var);
+		ft_lstadd_back(&lst_env, new);
+	}
+
+	// TEST DISPLAY LIST ELEM
+	t_var *env_var;
+	env_var = NULL;
+	env_var = (t_var *)lst_env->content;
+	printf("var_name: %s\n", env_var->name);
+	printf("var_value: %s\n", env_var->value);
+}
+
+int main(int ac, char **av, char **env)
+{
+	t_ms ms;
+	size_t buf_size = 1024;
+	char *buf;
+
+	(void)ac;
+	(void)av;
+	ms.env = NULL;
+	list_env(ms.env, env);
+	buf = malloc(sizeof(char) * buf_size);
+	if(!buf)
+	{
+		buf = NULL;
+		return (EXIT_FAILURE);
+	}
+	write(1, "$>", 2);
+	while(read(STDIN_FILENO, buf, buf_size) > 0)
+	{
+		write(1, "$>", 2);
+	}
+	free(buf);
+	return (0);
+}
