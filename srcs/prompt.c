85df5c2643a09a386f5ce1cef0c3149787b1be0d
bahaas <baudoin.haas@gmail.com>
2021-03-23 16:57:03 +0100

    FIX: prompt display with unset PWD value

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 13:56:39 by clorin            #+#    #+#             */
-/*   Updated: 2021/03/22 22:03:07 by bahaas           ###   ########.fr       */
+/*   Updated: 2021/03/23 16:55:17 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int			prompt(t_ms *ms)
 {
 	int ret;
 
-	ret = ft_strlen(ft_getenv(&ms->env, "PWD"));
-	write (1, ft_getenv(&ms->env, "PWD"), ret);
+	ret = ft_strlen(ms->pwd);
+	write (1, ms->pwd, ret);
 	write (1, "$> ", 3);
 	return (ret + 3);
 }
