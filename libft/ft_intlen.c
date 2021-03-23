eade8a26d2cb8e49b9730e696fb529855193657e
clorin <cyrille.lorin@yahoo.fr>
2021-03-23 10:21:05 +0100

    RFCT: ft_intlen(__intmax_t)

--- libft/ft_intlen.c
+++ libft/ft_intlen.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-size_t		ft_intlen(int nb)
+size_t			ft_intlen(__intmax_t nb)
 {
-	size_t	len;
+	size_t		len;
 
 	len = 0;
-	if (nb == 0)
-		len = 1;
-	while (nb > 0)
+	if (!nb)
+		len++;
+	while (nb)
 	{
 		nb = nb / 10;
 		len++;
