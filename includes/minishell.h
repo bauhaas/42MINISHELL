0d6bcabd62fbe77b82294d3cf7010987dfce5fb4
Cyrille LORIN <clorin@e3r13p4.42.fr>
2021-03-24 12:10:24 +0100

    ADD: ft_lstswap() in minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -142,6 +142,7 @@ char	*ft_getenv_name(t_list **head_ref, char *elem);
 void	print_env(t_var *env, int mod);
 
 void	init_ms(t_ms *ms, char **env);
+void	ft_lstswap(t_list *prev, t_list *next);
 void	tmp_line_to_cmd(t_ms *ms, char *line);
 
 int		ft_echo(t_ms *ms, t_cmd *cmd);
