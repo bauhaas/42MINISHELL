864623527ef23da8652b31265c3f431dd2bfde25
clorin <clorin@student.42.fr>
2021-03-31 14:47:59 +0200

    ADD: ~ to replace HOME directory in prompt()

--- srcs/termcaps/prompt.c
+++ srcs/termcaps/prompt.c
@@ -12,18 +12,43 @@
 
 #include "../includes/minishell.h"
 
+static char		*home_prompt(t_ms *ms, char *pt)
+{
+	char	*dest;
+	char	*home;
+	int		i;
+
+	i = 0;
+	home = ft_getenv(&ms->env, "HOME", TRUE);
+	while (home[i] == pt[i])
+		i++;
+	if (i > 0)
+	{
+		dest = ft_strnew(ft_strlen(pt + i) + 1);
+		dest[0] = '~';
+		ft_strcat(dest, pt + i);
+	}
+	else
+		dest = ft_strdup(ms->pwd);
+	return (dest);
+}
+
 int			prompt(t_ms *ms)
 {
-	int ret;
+	int 	ret;
+	char	*prompt_;
 
-	ret = ft_strlen(ms->pwd);
+	//ret = ft_strlen(ms->pwd);
 	char checkmark[] = { 0xe2, 0x9c, 0x93 };
 	char cross[] = { 0xe2, 0x9d, 0x8c };
 	//if(!ms->i)
 	//{
-		write (1, ms->pwd, ret);
+	prompt_ = home_prompt(ms, ms->pwd);
+	ret = ft_strlen(prompt_);		//write (1, ms->pwd, ret);
+	write(1, prompt_, ret);
 		write (1, "$> ", 3);
-		return (ret + 3);
+	ft_strdel(&prompt_);
+	return (ret + 3);
 	//}
 	/*
 	if(ms->cmd->ret_value)
