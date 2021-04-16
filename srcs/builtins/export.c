d621da7d05ffd02802c8e516d60a2d9b9fc80acd
clorin <cyrille.lorin@yahoo.fr>
2021-04-16 16:33:06 +0200

    RFCT: refacto arg_to_var() ...in progress

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -48,7 +48,88 @@ void		*list_sort(t_list **dest, t_list *list)
 	return (dest);
 }
 
-int			arg_to_var(char *arg, t_ms *ms)
+static int	has_not_egal(char *arg, t_ms *ms)
+{
+	t_var	*new;
+
+	if (!valid_name(arg))
+	{
+		ft_putstr_fd("Minishell: export: `", STDERR);
+		ft_putstr_fd(arg, STDERR);
+		ft_putstr_fd("': not a valid identifier\n", STDERR);
+		return (1);
+	}
+	if (!ft_getenv(&ms->env, arg, FALSE))
+	{
+		new = malloc(sizeof(t_var));
+		if (new)
+		{
+			new->name = ft_strdup(arg);
+			new->value = NULL;
+			ft_lstadd_back(&ms->env, ft_lstnew(new));
+		}
+		else
+			return (1);
+	}
+	return (0);
+}
+
+static int	export_ok(char **arg_split, t_ms *ms)
+{
+	t_var	*new;
+
+	new = ft_get_t_var(&ms->env, arg_split[0]);
+	if (!new)
+	{
+		new = malloc(sizeof(t_var));
+		if (!new)
+			return (1);
+		new->name = ft_strdup(arg_split[0]);
+		if (arg_split[1] != NULL)
+			new->value = ft_strdup(arg_split[1]);
+		else
+			new->value = ft_strnew(0);
+		ft_lstadd_back(&ms->env, ft_lstnew(new));
+	}
+	else
+	{
+		ft_strdel(&new->value);
+		if (arg_split[1] != NULL)
+			new->value = ft_strdup(arg_split[1]);
+		else
+			new->value = ft_strnew(0);
+	}
+	return (0);
+}
+
+static int	arg_to_var(char *arg, t_ms *ms)
+{
+	char	**arg_split;
+	int		ret;
+
+	if (ft_strchr(arg, '='))
+	{
+		arg_split = ft_split(arg, '=');
+		if (valid_name(arg_split[0]))
+		{
+			ret = export_ok(arg_split, ms);
+			free_split(&arg_split);
+			return (ret);
+		}
+		else
+		{
+			ft_putstr_fd("Minishell: export: `", STDERR);
+			ft_putstr_fd(arg, STDERR);
+			ft_putstr_fd("': not a valid identifier\n", STDERR);
+			return (1);
+		}
+	}
+	else
+		return (has_not_egal(arg, ms));
+}
+
+/*
+int			arg_to_var2(char *arg, t_ms *ms)
 {
 	char	**arg_split;
 	t_var	*new;
@@ -114,7 +195,7 @@ int			arg_to_var(char *arg, t_ms *ms)
 		}
 	}
 	return (0);
-}
+}*/
 
 int			ft_export(t_ms *ms, t_cmd *cmd)
 {
