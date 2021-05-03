ea8eeb4a0ecbe432ef53ce5f0e643766ff9bd0c2
bahaas <baudoin.haas@gmail.com>
2021-05-03 11:16:52 +0200

    ADD: redir in loop after cmd

--- srcs/command.c
+++ srcs/command.c
@@ -6,7 +6,7 @@
 /*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 14:52:26 by bahaas            #+#    #+#             */
-/*   Updated: 2021/04/30 21:35:33 by bahaas           ###   ########.fr       */
+/*   Updated: 2021/05/03 11:14:21 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -176,7 +176,6 @@ int			last_cmd_status(t_ms *ms, t_cmd *cmd)
 	tmp = cmd;
 	if (!strcmp(tmp->content[0], ";") && !tmp->next)
 	{
-		printf("test ;\n");
 		printf("minishell: syntax error near unexpected token");
 		printf(" « %s »\n", tmp->content[0]);
 		ms->last_ret = 2;
@@ -188,19 +187,14 @@ int			last_cmd_status(t_ms *ms, t_cmd *cmd)
 						|| tmp->type_link == 8) && tmp->next == NULL))
 			return (print_cmd_error(ms, tmp));
 		if(is_redir(tmp) && (tmp->next && is_redir(tmp->next)))
-		{
-			printf("test\n");
 			return (print_cmd_error(ms, tmp));
-		}
 		else if(ms->total_consecutive_pipes == 2)
 		{
-			printf("tast\n");
 			printf("minishell: syntax error near unexpected token  « | »\n");
 			return (0);
 		}
 		else if(ms->total_consecutive_pipes > 2)
 		{
-			printf("tost\n");
 			printf("minishell: syntax error near unexpected token  « || »\n");
 			return (0);
 		}
@@ -257,7 +251,6 @@ static t_cmd	*next_cmd_to_execute(t_ms *ms, t_cmd *cmd)
 	}
 	if(DEBUG)
 	{
-
 		if(cmd)
 			printf("return cmd : %s\n", cmd->content[0]);
 		else
@@ -322,6 +315,51 @@ static void waiting_loop(t_ms *ms, t_cmd *cmd)
 			printf("pas de Fork \t ms->last_ret = %d\n", ms->last_ret);
 	}
 }
+static void set_redirection(t_ms *ms, t_cmd *cmd)
+{
+	if(DEBUG)
+		printf("cmd enter in set_redir : %s\n", cmd->content[0]);
+	t_cmd *first_cmd;
+	t_cmd *redir_cmd;
+	t_cmd *file_to_redirect;
+
+	first_cmd = cmd;
+	if(cmd && (cmd->next && is_redir(cmd->next)))
+	{
+		redir_cmd = cmd->next;
+		if(redir_cmd->next)
+			file_to_redirect = redir_cmd->next;
+	}
+	if(DEBUG)
+	{
+		if(redir_cmd)
+			printf("cmd_redir : %s\n", redir_cmd->content[0]);
+		if(file_to_redirect)
+			printf("file_to_redirect : %s\n", file_to_redirect->content[0]);
+	}
+	while((redir_cmd && is_redir(redir_cmd)))
+	{
+		if (is_type(redir_cmd, DRIGHT))
+			redir(ms, file_to_redirect, DRIGHT);
+		else if (is_type(redir_cmd, RIGHT))
+			redir(ms, file_to_redirect, RIGHT);
+		else if (is_type(redir_cmd, LEFT))
+			input(ms, file_to_redirect);
+		if(ms->ret == 1)
+			break;
+		if(redir_cmd)
+		{
+			if(redir_cmd->next && redir_cmd->next->next)
+				redir_cmd = redir_cmd->next->next;
+			else
+				break;
+			if(file_to_redirect->next && file_to_redirect->next->next)
+				file_to_redirect = file_to_redirect->next->next;
+			else
+				break;
+		}
+	}
+}
 
 static void pipeline(t_cmd *cmd, t_ms *ms)
 {
@@ -336,10 +374,8 @@ static void pipeline(t_cmd *cmd, t_ms *ms)
 	ms->forked = 0;
 	if (DEBUG)
 		printf("Pipeline : minishell pid : %d\n", getpid());
-
 	while(cmd && is_redir(cmd))
 	{
-		//	printf("cmd in redir loop : %s\n", cmd->content[0]);
 		if (is_type(cmd, DRIGHT))
 			redir(ms, cmd->next, DRIGHT);
 		else if (is_type(cmd, RIGHT))
@@ -358,10 +394,15 @@ static void pipeline(t_cmd *cmd, t_ms *ms)
 	if(has_redir_first && is_type(cmd, PIPES) && cmd->next)
 		cmd = cmd->next;
 	reset_fd(ms);
-
 	if(DEBUG)
+	{
 		if(cmd)
+		{
 			printf("cmd before pipe loop : %s\n", cmd->content[0]);
+			printf("ms->ret : %d\n", ms->ret);
+		}
+		printf("ms->ret : %d\n", ms->ret);
+	}
 	while (cmd && !ms->ret)
 	{
 		if (DEBUG)
@@ -374,25 +415,6 @@ static void pipeline(t_cmd *cmd, t_ms *ms)
 			;
 		else
 		{
-			//// TRY REDIR BEFORE FORK
-			/*
-			t_cmd *file_to_redirect;
-			if(cmd->next)
-			{
-				if(cmd->next->next)
-					file_to_redirect = cmd->next->next;
-				if(file_to_redirect)
-				{
-
-					if (is_type(file_to_redirect->prev, DRIGHT))
-						redir(ms, cmd, DRIGHT);
-					else if (is_type(file_to_redirect->prev, RIGHT))
-						redir(ms, cmd, RIGHT);
-					else if (is_type(file_to_redirect->prev, LEFT))
-						input(ms, cmd);
-				}
-			}
-			*/
 			ms->forked++;
 			pipe(fd);
 			pid = fork();
@@ -404,86 +426,22 @@ static void pipeline(t_cmd *cmd, t_ms *ms)
 			else if (pid == 0)
 			{
 				/*
-				close_fd(fd[1]);
-				dup2(fd[0], STDIN);
-				ms->pipin = fd[0];
-*/
+				   close_fd(fd[1]);
+				   dup2(fd[0], STDIN);
+				   ms->pipin = fd[0];
+				   */
 				if (DEBUG)
 					printf("fils(%d) cmd = %s\n",getpid(), cmd->content[0]);
 				dup2(fdd, STDIN);
 				if (cmd->next && cmd->next->type_link == 4)
 					dup2(fd[1], STDOUT);
 				if (cmd->next && is_redir(cmd->next))
-				{
-					//fd_open=fonctionsuperdrole(cmd->next)
-					/*fonction
-					  ouvre les fichiers >
-					  renvoi le fd_open
-					  int fd_open = open(cmd->next->next->content[0], O_CREAT | O_WRONLY | O_APPEND, S_IRWXU);*/
-					//dup2(fd_open, STDOUT);
-					//int fd_open;
-					t_cmd *file_to_redirect;
-
-					if(cmd->next->next)
-						file_to_redirect = cmd->next->next;
-					//	while(file_to_redirect)
-					//	{
-
-					/*
-					   printf("test file to redir : %s\n", file_to_redirect->content[0]);
-					   printf("test file to redir next : %s\n", file_to_redirect->next->next->content[0]);
-					   printf("test file to redir next next : %s\n", file_to_redirect->next->next->content[0]);
-					   */
-					
-					   if(is_type(file_to_redirect->prev, RIGHT))
-					   {
-					   ft_close(fd_open);
-					   fd_open = open(file_to_redirect->content[0], O_CREAT | O_WRONLY | O_APPEND, S_IRWXU);
-					   dup2(fd_open, STDOUT);
-					   }
-					   else if(is_type(file_to_redirect->prev, DRIGHT))
-					   {
-					   ft_close(fd_open);
-					   fd_open = open(file_to_redirect->content[0], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-					   dup2(fd_open, STDOUT);
-					   }
-					   else if(is_type(file_to_redirect->prev, LEFT))
-					   {
-					   ft_close(fd_open);
-					   fd_open = open(file_to_redirect->content[0], O_RDONLY, S_IRWXU);
-					   dup2(fd_open, STDIN);
-					   }
-					  
-					   ///TEST PR LOOP
-					   /*
-						if(file_to_redirect->next->next)
-						{
-						//	ft_close(fd_open);
-						//	dup2(fd[1], STDOUT);
-						printf("next file to redirect to : %s\n", file_to_redirect->next->next->content[0]);
-						file_to_redirect = file_to_redirect->next->next;
-						}
-						*/
-					/*
-					if(file_to_redirect->next->next)
-					{
-					//printf("next file to redirect to : %s\n", file_to_redirect->next->next->content[0]);
-					file_to_redirect = file_to_redirect->next->next;
-					}
-					*/
-
-
-					/*
-					else
-					{
-					//printf("no other file to redirect\n");
-					break;
-					}
-					*/
-				}
-
+					set_redirection(ms, cmd);
+				if(DEBUG)
+					printf("ms->ret after redir : %d\n", ms->ret);
+				if(ms->ret)
+					exit(ms->ret);
 				close(fd[0]);
-
 				if(DEBUG)
 					printf("Check cmd before bltn or exec : %s\n", cmd->content[0]);
 				cmd_is_bltn(ms, cmd, 1);
@@ -500,68 +458,68 @@ static void pipeline(t_cmd *cmd, t_ms *ms)
 					exit(ms->last_ret);
 				}
 				execve(cmd->content[0], cmd->content, ms->arr_env);
-			}
-			else
-			{
-				//reset_fd(ms);
+				}
+				else
+				{
+					//reset_fd(ms);
 
-				close(fd[1]);
-				fdd = fd[0];
-				//ft_close(fd_open);
+					close(fd[1]);
+					fdd = fd[0];
+					//ft_close(fd_open);
+				}
 			}
+			cmd = next_cmd_to_execute(ms, cmd);
+			if(DEBUG)
+				if(cmd && cmd->content[0])
+					printf("next cmd that will be executed : %s\n\n", cmd->content[0]);
 		}
-		cmd = next_cmd_to_execute(ms, cmd);
-		if(DEBUG)
-			if(cmd && cmd->content[0])
-				printf("next cmd that will be executed : %s\n\n", cmd->content[0]);
+		waiting_loop(ms, cmd);
+		if (DEBUG)
+			printf("sortie Pipeline\n");
 	}
-	waiting_loop(ms, cmd);
-	if (DEBUG)
-		printf("sortie Pipeline\n");
-}
 
-static int	nb_cmd(t_cmd *cmd)
-{
-	int n_command_to_exec = 0;
-	t_cmd *tmp;
-
-	tmp = cmd;
-	while(tmp)
+	static int	nb_cmd(t_cmd *cmd)
 	{
-		if (tmp->type_link != 4)
-			n_command_to_exec++;
-		tmp = tmp->next;
+		int n_command_to_exec = 0;
+		t_cmd *tmp;
+
+		tmp = cmd;
+		while(tmp)
+		{
+			if (tmp->type_link != 4)
+				n_command_to_exec++;
+			tmp = tmp->next;
+		}
+		return (n_command_to_exec);
 	}
-	return (n_command_to_exec);
-}
 
-void		line_to_cmd(t_ms *ms, char *line, t_cmd *cmd)
-{
-	t_tokens *head;
-	t_cmd *tmp;
-	t_cmd *to_free;
-
-	ms->tokens = NULL;
-	ms->total_consecutive_pipes = 0;
-	cmd = NULL;
-	parse(line, ms);
-	if (DEBUG >= 3)
-		print_tokens(ms->tokens);
-	head = ms->tokens;
-	while (head)
-		tokens_to_cmd(ms, &cmd, &head);
-	free_tokens(ms->tokens);
-	if (DEBUG)
-		print_cmd(cmd);
-	to_free = cmd;
-	tmp = cmd;
-	//printf("ms->total_consecutive_pipes : %d\n", ms->total_consecutive_pipes);
-	if (nb_cmd(cmd) > 1 || (nb_cmd(cmd) == 1 && !get_bltn(ms, cmd->content[0])))
+	void		line_to_cmd(t_ms *ms, char *line, t_cmd *cmd)
 	{
-		if(last_cmd_status(ms, cmd))
-			pipeline(tmp, ms);
+		t_tokens *head;
+		t_cmd *tmp;
+		t_cmd *to_free;
+
+		ms->tokens = NULL;
+		ms->total_consecutive_pipes = 0;
+		cmd = NULL;
+		parse(line, ms);
+		if (DEBUG >= 3)
+			print_tokens(ms->tokens);
+		head = ms->tokens;
+		while (head)
+			tokens_to_cmd(ms, &cmd, &head);
+		free_tokens(ms->tokens);
+		if (DEBUG)
+			print_cmd(cmd);
+		to_free = cmd;
+		tmp = cmd;
+		//printf("ms->total_consecutive_pipes : %d\n", ms->total_consecutive_pipes);
+		if (nb_cmd(cmd) > 1 || (nb_cmd(cmd) == 1 && !get_bltn(ms, cmd->content[0])))
+		{
+			if(last_cmd_status(ms, cmd))
+				pipeline(tmp, ms);
+		}
+		else
+			cmd_is_bltn(ms, cmd, 0);
+		free_cmd(to_free);
 	}
-	else
-		cmd_is_bltn(ms, cmd, 0);
-	free_cmd(to_free);
-}
